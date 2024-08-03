43d8aa5a32470b9207d5a5bbb8baa458824cd4d3
ksawerykowalczuk <ksawery.kowalczuk@gmail.com>
2024-08-03 21:37:01 +0200

    NIE

--- malyjas.cpp
+++ malyjas.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main()
@@ -6,7 +7,90 @@ int main()
     int rozmiar;
     string wejscie;
     string docelowy;
+    cin >> rozmiar;
     cin >> wejscie;
     cin >> docelowy;
-    
+    int i = 0;
+    int licznik = 0;
+    while (wejscie != docelowy)
+    {
+        if (wejscie[i] == docelowy[i])
+        {
+            i++;
+        }
+        else
+        {
+            int j = 0;
+            while (j < rozmiar)
+            {
+                if (wejscie[i] == docelowy[j])
+                {
+                    for (int k = 0; k < abs(j - i); k++)
+                    {
+                        if (i > j)
+                        {
+                            char temp = docelowy[j+k - 1];
+                            docelowy[j+k - 1] = docelowy[j+k];
+                            docelowy[j+k] = temp;
+                        }
+                        else
+                        {
+                            char temp = docelowy[j-k];
+                            docelowy[j-k] = docelowy[j-k - 1];
+                            docelowy[j-k - 1] = temp;
+                        }
+                    }
+
+                    break;
+                }
+                else
+                {
+                    j++;
+                }
+            }
+        }
+    }
+    cout << licznik;
 }
+
+
+
+/*
+ABC
+BAC
+BCA
+
+5
+ABCDE
+BACDE
+BADCE
+2
+
+5
+ABCDE
+BACDE
+BADCE
+BADEC
+BAEDC
+BEADC
+EBADC
+EBADC
+7
+
+ABCDE
+ABCED
+ABECD
+AEBCD
+EABCD
+EBACD
+EBADC
+6
+
+
+ABCDE
+baCDE
+BcaDE
+cbADE
+
+EBADC
+*/
